09c53a0ed5039ddbebe646f99d1fc0d16e219a3d
Juliya-L <gilukju2@gmail.com>
2026-06-24 13:11:04 +0300

    feat: register models in Django admin

--- hotels/admin.py
+++ hotels/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Hotel, Room, Guest, Booking
 
-# Register your models here.
+
+admin.site.register(Hotel)
+admin.site.register(Room)
+admin.site.register(Guest)
+admin.site.register(Booking)
+
