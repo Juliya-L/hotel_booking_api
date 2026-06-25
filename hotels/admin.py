d5f7c2af242bc595be0509ddf1e64f72b762d14f
Juliya-L <gilukju2@gmail.com>
2026-06-25 17:50:33 +0300

    feat: customize Django admin with list_display, filters and search

--- hotels/admin.py
+++ hotels/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
 from .models import Hotel, Room, Guest, Booking
 
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+  list_display = ['name', 'city']
+  search_fields = ['name', 'city']
 
-admin.site.register(Hotel)
-admin.site.register(Room)
-admin.site.register(Guest)
-admin.site.register(Booking)
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+  list_display = ['hotel', 'number', 'room_type', 'price_per_night' ]
+  list_filter = ['room_type']
+  search_fields = ['number']
+
+
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+  list_display = ['full_name', 'email', 'phone']
+  search_fields = ['phone']
+  
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+  list_display = ['guest', 'room', 'check_in', 'check_out']
+  list_filter = ['status']
+
 
