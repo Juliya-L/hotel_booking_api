e72be4348cd32b2d0b5136c0bdad9fbac54c7f93
Juliya-L <gilukju2@gmail.com>
2026-08-12 21:09:50 +0300

    feat: add ViewSets and API routing for all models

--- hotels/views.py
+++ hotels/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Hotel, Room, Guest, Booking
+from .serializers import HotelSerializer, RoomSerializer, GuestSerializer, BookingSerializer
 
-# Create your views here.
+
+class HotelViewSet(viewsets.ModelViewSet):
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
+
+
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class GuestViewSet(viewsets.ModelViewSet):
+    queryset = Guest.objects.all()
+    serializer_class = GuestSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
