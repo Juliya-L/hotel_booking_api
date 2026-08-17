546af2c6e7b720e45dc8022da6deb18be863fa4d
Juliya-L <gilukju2@gmail.com>
2026-08-17 11:43:35 +0300

    feat: filter available rooms by check_in and check_out dates

--- hotels/views.py
+++ hotels/views.py
@@ -13,6 +13,24 @@ class RoomViewSet(viewsets.ModelViewSet):
     serializer_class = RoomSerializer
 
 
+    def get_queryset(self):
+        queryset = Room.objects.all()
+
+        check_in = self.request.query_params.get('check_in')
+        check_out = self.request.query_params.get('check_out')
+
+        if check_in and check_out:
+            booked_room_ids = Booking.objects.filter(
+                check_in__lt=check_out,
+                check_out__gt=check_in,
+            ).exclude(status='cancelled').values_list('room_id', flat=True)
+
+            queryset = queryset.exclude(id__in=booked_room_ids)
+
+        return queryset
+
+
+
 class GuestViewSet(viewsets.ModelViewSet):
     queryset = Guest.objects.all()
     serializer_class = GuestSerializer
